9588d7cc5cd9ec3ad95291768030f7d5a0723e4c
Nataly <mvnataly@yandex.ru>
2021-05-31 13:10:26 +0300

    Создание скелета проекта

---
+++ ShopApi/Models/Spravochnik/AllProductsModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApi.Models.Spravochnik
+{
+    /// <summary>
+    /// Справочная информация по товарам
+    /// </summary>
+    public class AllProductsModel
+    {
+       public int bookid { get; set; }
+        
+       public string bookname { get; set; }
+
+        public string categotyname { get; set; }
+        public int tgid { get; set; }
+    }
+}
