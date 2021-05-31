9824c8db2a52d11e75b0b2280315144372613623
Nataly <mvnataly@yandex.ru>
2021-05-31 13:24:42 +0300

    Структура для справочника

--- Infrastructure/Spravochnik/Spravochnik.cs
+++ Infrastructure/Spravochnik/Spravochnik.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ShopApi.Models.Spravochnik;
+using ShopApi.Infrastructure;
+using System.Threading.Tasks;
 
 namespace Infrastructure.Spravochnik
 {
-   public class Spravochnik
+    public class Spravochnik : ISpravochnik
     {
+        public Task<IEnumerable<AllProductsModel>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ShopApi/Infrastructure/ISpravochnik.cs
+++ ShopApi/Infrastructure/ISpravochnik.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Threading.Tasks;
+
+using ShopApi.Models.Spravochnik;
 
 namespace ShopApi.Infrastructure
 {
-    interface ISpravochnik
+    public interface ISpravochnik
     {
+        /// <summary>
+        /// Получить список всех продуктов
+        /// </summary>
+        /// <returns></returns>
+        public Task<IEnumerable<AllProductsModel>> GetAllProducts();
+
+        
     }
 }
